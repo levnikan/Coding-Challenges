471d77230cf4e650405802d9fd16229a24beaa8f
Nikolay Levchenko <nik.and.levchenko@gmail.com>
2019-01-18 19:30:39 +0100

    Added flat map example

--- src/main/java/telran/de/abstractClasses/Main.java
+++
@@ -1,7 +0,0 @@
-package telran.de.abstractClasses;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/telran/de/streamAPI/Main.java
@@ -0,0 +1,24 @@
+package telran.de.streamAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("One flew over the cuckoo's nest");
+        stringList.add("To kill a muckingbird");
+        stringList.add("Gone with the wind");
+
+        Stream<String> stream = stringList.stream();
+
+        stream.flatMap((value) -> {
+            String[] split = value.split(" ");
+            return Arrays.stream(split);
+        })
+                .forEach(System.out::println); // Alternative to .forEach(value -> System.out.println(value));
+    }
+}
